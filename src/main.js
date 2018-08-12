dee9f971e2fea921c54c3011551559528f925027
lookInward <1521274537@qq.com>
2018-08-12 20:15:45 +0800

    添加vant-ui组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 按需引入
+import { Button } from 'vant'
+Vue.use(Button)
+// 全局引入
+// import Vant from 'vant'
+// import 'vant/lib/vant-css/index.css'
+// Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
